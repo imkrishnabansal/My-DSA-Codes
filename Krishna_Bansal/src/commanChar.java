9898da05f319c0064f17134584245b857bc7ba17
krishnabansal454 <122722493+krishnabansal454@users.noreply.github.com>
2024-06-07 12:50:21 +0530

    7th june 2024

--- Krishna_Bansal/src/commanChar.java
+++ Krishna_Bansal/src/commanChar.java
@@ -1,13 +1,13 @@
-package Krishna_Bansal.src;
-import java.util.*;
-public class commanChar {
-    public static void main(String[] args) {
-        String []words = {"bella","label","roller"};
-        for(int i =0;i<words.length;i++){
-            for(int j =i+1;j<words.length;j++){
-             char ch = words
-            }
-        }
-    }
+//package Krishna_Bansal.src;
+//import java.util.*;
+//public class commanChar {
+//    public static void main(String[] args) {
+//        String []words = {"bella","label","roller"};
+//        for(int i =0;i<words.length;i++){
+//            for(int j =i+1;j<words.length;j++){
+//             char ch = words;
+//            }
+//        }
+//    }
 
-}
+//}
