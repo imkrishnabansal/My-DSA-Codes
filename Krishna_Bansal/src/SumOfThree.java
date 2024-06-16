aff1fa92802e1d171d1d808edbd403e8d9593cc0
krishnabansal454 <122722493+krishnabansal454@users.noreply.github.com>
2024-06-17 00:58:18 +0530

    16th june2024
    dsa codes

--- Krishna_Bansal/src/SumOfThree.java
+++ Krishna_Bansal/src/SumOfThree.java
@@ -4,42 +4,32 @@ import java.util.Scanner;
 
 public class SumOfThree {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // Create a scanner object to read input from the console
-        int t = sc.nextInt(); // Read the number of test cases
-
-        // Loop through each test case
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
         for (int testCase = 0; testCase < t; testCase++) {
-            int n = sc.nextInt(); // Read the integer n for the current test case
-
+            int n = sc.nextInt();
             if (n < 6) {
-                // If n is less than 6, it's impossible to find such a triplet
                 System.out.println("NO");
             } else {
-                // We need to find x, y, and z such that x + y + z = n
-                // and none of x, y, z is divisible by 3
                 boolean found = false;
 
-                // Try some combinations of x, y, and z
                 for (int x = 1; x <n - 1 && !found; x++) {
-                    if (x % 3 == 0) continue; // Skip x if it's divisible by 3
+                    if (x % 3 == 0) continue;
                     for (int y = x + 1; y <n && !found; y++) {
-                        if (y % 3 == 0) continue; // Skip y if it's divisible by 3
-                        int z = n - x - y; // Compute z
-                        if (z > y && z % 3 != 0 && z != x && z != y) { // z must be distinct and not divisible by 3
+                        if (y % 3 == 0) continue;
+                        int z = n - x - y;
+                        if (z > y && z % 3 != 0 && z != x && z != y) {
                             System.out.println("YES");
                             System.out.println(x + " " + y + " " + z);
                             found = true;
                         }
                     }
                 }
-
                 if (!found) {
                     // If no such triplet is found
                     System.out.println("NO");
                 }
             }
         }
-
-        sc.close(); // Close the scanner
     }
 }
---
+++ codeforces/watermelon.java
@@ -0,0 +1,15 @@
+package codeforces;
+import java.util.*;
+public class watermelon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int w =sc.nextInt();
+        if(w%2==0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+
+    }
+}
